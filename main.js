ebee5ac4bcfaed6d4ad2b2e92ade431cdbcfd4e4
marwenez <labidimarwen6@gmail.com>
2022-01-21 17:08:18 +0100

    #PWA : i setup the configuraion file and i create the instalation and the precache functionality

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ AOS.init({
 	delay: 10,
 	duration: 700
 });
+import {
+	registerSW
+} from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+	onNeedRefresh() {alert('Need refresh')},
+	onOfflineReady() {alert('Offline ready')},
+})
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import {
 	defineConfig
 } from 'vite'
-
 import {
 	minifyHtml
 } from 'vite-plugin-html'
+import {
+	VitePWA
+} from 'vite-plugin-pwa'
 
 
 export default defineConfig({
@@ -14,6 +16,33 @@ export default defineConfig({
 	},
 	plugins: [
 		minifyHtml(),
+		VitePWA({
+			includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+			manifest: {
+				name: 'HEARING AID REPAIR SHOP',
+				short_name: 'HEARING SHOP',
+				description: 'repair all your hearing devices problems',
+				theme_color: '#FFA500',
+				icons: [{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					}
+				]
+			},
+			registerType: 'autoUpdate',
+		})
 	],
 
 })
