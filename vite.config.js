87ee6f3243027944986fd3665798ade6decbf569
marwenez <labidimarwen6@gmail.com>
2022-01-21 19:09:38 +0100

    #fix #PWA : i fixed the static import of image and the cache of the image and i create the dynamic import

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
 	plugins: [
 		minifyHtml(),
 		VitePWA({
-			includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+			includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png','photo1.jpeg','photo2.jpeg','photo3.jpeg','64.jpg','62.jpg','63.jpg','61.jpg'],
 			manifest: {
 				name: 'HEARING AID REPAIR SHOP',
 				short_name: 'HEARING SHOP',
@@ -42,6 +42,37 @@ export default defineConfig({
 				]
 			},
 			registerType: 'autoUpdate',
+			workbox: {
+				runtimeCaching: [{
+						urlPattern: /https:\/\/unpkg\.com\/aos@next\/dist\//i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'aos-library-cache',
+							expiration: {
+								maxEntries: 10,
+								maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+							},
+							cacheableResponse: {
+								statuses: [0, 200]
+							}
+						}
+					},
+					{
+						urlPattern: /https:\/\/cdnjs\.cloudflare\.com\/ajax\/libs\/font-awesome\/5\.13\.0\/css\/all\.min\.css/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'icon-font-awesome-cache',
+							expiration: {
+								maxEntries: 10,
+								maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+							},
+							cacheableResponse: {
+								statuses: [0, 200]
+							},
+						}
+					}
+				]
+			}
 		})
 	],
 
